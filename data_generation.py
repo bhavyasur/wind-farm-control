7a96cb9251fe85dd54c472751e6bab89e8019995
bhavyasurapan <108424704+bhavyasurapan@users.noreply.github.com>
2025-12-07 09:30:05 -0500

    sat night

--- data_generation.py
+++ data_generation.py
@@ -1,4 +1,14 @@
 import floris
-import
+import matplotlib.pyplot as plt
+from floris import FlorisModel
+fmodel = FlorisModel("model name here") # model is instantiated, can be used to inspect data
+x, y = fmodel.get_turbine_layout()
+
+num_turbines = 3
+x_2x2 = [0, 400, 0, 800]
+y_2x2 = [0, 0, 800, 800]
+fmodel.set(layout_x=x_2x2, layout_y=y_2x2)
+
+
 
 turbine_type = "nrel_5MW"
