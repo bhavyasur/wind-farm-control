695c55fa86676d3776b18790c2179fc26eeef897
bhavyasurapan <108424704+bhavyasurapan@users.noreply.github.com>
2025-12-07 14:30:06 -0500

    fix for run issue

--- data_generation.py
+++ data_generation.py
@@ -1,8 +1,16 @@
-import floris
+import numpy as np
 import matplotlib.pyplot as plt
-from floris import FlorisModel
-fmodel = FlorisModel("model name here") # model is instantiated, can be used to inspect data
+
+from floris import FlorisModel, TimeSeries
+import floris.flow_visualization as flowviz
+import floris.layout_visualization as layoutviz
+
+fmodel = FlorisModel("farm_types/gch.yaml") # model is instantiated, can be used to inspect data
 x, y = fmodel.get_turbine_layout()
+print("initial turbine layout")
+print("     x       y")
+for _x, _y in zip(x, y):
+    print(f"{_x:6.1f}, {_y:6.1f}")
 
 num_turbines = 3
 x_2x2 = [0, 400, 0, 800]
